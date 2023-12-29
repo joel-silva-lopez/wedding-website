74134f8ab1eccc27fd3a17cb7ba66d53ee3462a5
joel-silva-lopez <joel_silva_lopez@hotmail.com>
2023-12-29 15:36:48 -0700

    Cambiando el numero

--- src/components/Errores/Error404.jsx
+++ src/components/Errores/Error404.jsx
@@ -5,7 +5,7 @@ export const Error404 = () => {
             <h1>Error 404: Página no encontrada</h1>
             <p>La invitacion que estás buscando no existe.</p>
             <p>Por favor comunicate con atencion a clientes si crees que es un error.</p>
-            <a href="https://wa.me/+5216672371132/?text=Buenas%20tardes,%20tengo%20un%20error%20con%20mi%20invitacion.">
+            <a href="https://wa.me/+5216672672868/?text=Buenas%20tardes,%20tengo%20un%20error%20con%20mi%20invitacion.">
                 Enviar mensaje
             </a>
         </div>
